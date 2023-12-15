889a0078a256a9fac38f37fb59b82721fc7b402b
maxx <maxxng1028@gmail.com>
2023-12-16 00:18:51 +0800

    feat: updated config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import("tailwindcss").Config} */
+export default {
+  content: ["./src/**/*.{html,css,svelte,ts}"],
+  daisyui: {
+    theme: ["night", "dark"]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}
