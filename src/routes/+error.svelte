cc258cfdaa59ee6c8fe1d253512b9e0ff78fef00
maxx <maxxng1028@gmail.com>
2023-12-16 00:29:00 +0800

    changes: changed colors of button

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -12,7 +12,7 @@
     <h1 class="font-bold text-3xl">Hmmm....  Looks like an error occured!</h1>
     <h2 class="font-bold text-lg">Error {$page.status} | {$page.error?.message}</h2>
     <div class="flex flex-row mt-5 gap-5">
-        <a class="btn btn-md btn-neutral" href="/">Go back</a>
-        <a class="btn btn-md btn-secondary" href="https://www.youtube.com/watch?v=xvFZjo5PgG0">Quick fix</a>
+        <a class="btn btn-md btn-primary" href="/">Go back</a>
+        <a class="btn btn-md btn-accent" href="https://www.youtube.com/watch?v=xvFZjo5PgG0">Quick fix</a>
     </div>
 </div>
